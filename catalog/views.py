29ec83d2d6fa7ea370bb033fd24429d7ae51c3ee
NikaKoipish <152218836+NikaKoipish@users.noreply.github.com>
2024-03-22 21:27:11 +0300

    Add files via upload

---
+++ catalog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def index_contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name} ({phone}): {message}')
+    return render(request, 'catalog/index_contacts.html')
+
+def index_home(request):
+    return render(request, 'catalog/index_home.html')

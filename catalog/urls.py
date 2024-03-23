90589adc5ff1476077772694a891118cc904d273
NikaKoipish <152218836+NikaKoipish@users.noreply.github.com>
2024-03-23 12:03:14 +0300

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.views import index_home, index_contacts
 urlpatterns = [
-    path('', index_contacts),
+    path('catalog/', index_contacts),
     path('', index_home)
 ]
